856fb45439845435a5134c915b01b5456b8ffd33
Marina Lopez <marinalopezka@gmail.com>
2026-05-02 23:32:25 -0300

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+
+    return (
+        <footer className={styles.footer}>
+            <p>© 2026 Tienda Yo Soy Yo - Todos los derechos reservados</p>
+        </footer>
+    )
+}
+
+export default Footer;
