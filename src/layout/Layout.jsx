900b067cbf41dbacf2ff1a455757e1500702fe14
Marina Lopez <marinalopezka@gmail.com>
2026-06-12 23:09:16 -0300

    style layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,17 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
+import styles from "./Layout.module.css";
 
 
 function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.layout}>
       <Header/>
 
-       <Outlet />
+       <main className={styles.main}>
+        <Outlet />
+      </main>
 
       <Footer/>
     </div>
