21504757c7127d3941801b3cccb6b96f695a057f
Marina Lopez <marinalopezka@gmail.com>
2026-06-13 12:09:10 -0300

    resolve import in Inicio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,4 +1,4 @@
-import ItemListContainer from "../../components/ItemListContainer/ItemListContainer";
+
 import NewProductContainer from "../../components/NewProductContainer/NewProductContainer";
 import { useNavigate} from "react-router-dom";
 import { useState, useEffect } from "react";
