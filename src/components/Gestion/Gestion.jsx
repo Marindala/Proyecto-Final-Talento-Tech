2294c4d6188d708f3f1fa8b7dbad4db6f3301ba6
Marina Lopez <marinalopezka@gmail.com>
2026-06-22 01:51:23 -0300

    module firebase

--- src/components/Gestion/Gestion.jsx
+++ src/components/Gestion/Gestion.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db } from '../../firebase/config';
+import { db } from '../../Firebase/config.js';
 import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import NewProductContainer from '../NewProductContainer/NewProductContainer';
 
