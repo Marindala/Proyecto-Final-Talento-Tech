6f9a605db5fd8c23794014270d8c5ef04bf6619d
Marina Lopez <marinalopezka@gmail.com>
2026-06-24 23:56:51 -0300

    include social links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,13 @@
 //import Directorio from "../Directorio/Directorio";
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import styles from "./Footer.module.css"
+
+const socialLinks = [
+  { icon: FaFacebook, href: "https://www.facebook.com/remerassublimadasyosoyyo/", label: "Facebook" },
+  { icon: FaInstagram, href: "https://www.instagram.com/tiendayosoyyo/", label: "Instagram" },
+  { icon: FaWhatsapp, href: "https://wa.me/message/", label: "Whatsapp" },
+ 
+];
 
 function Footer() {
 
@@ -12,6 +20,19 @@ function Footer() {
             <p className={styles.slogan}>
                 Realizado con Amor ♡ 
             </p>
+             <div className={styles.socialContainer}>
+            {socialLinks.map((social) => (
+              <a
+                key={social.label}
+                target="_blank"
+                href={social.href}
+                aria-label={social.label}
+                 className={styles.socialLink}
+              >
+                <social.icon className={styles.icon} />
+              </a>
+            ))}
+          </div>
 
             <p className={styles.copy}>© 2026 Tienda Yo Soy Yo - Todos los derechos reservados</p>
         </footer>
