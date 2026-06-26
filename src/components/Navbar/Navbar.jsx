7ea4eb178707e6aaaa1158892263f0fa72442ec7
Marina Lopez <marinalopezka@gmail.com>
2026-06-26 01:40:28 -0300

    total products in cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { useState } from "react";
 import styles from "./Navbar.module.css"
 import { Link } from 'react-router-dom';
+import { useCart } from "../../context/CartContext";
 
 function Navbar({ menuOpen, setMenuOpen }) {
+    const { getCartQuantity } = useCart();
+    console.log("Cantidad en el carrito:", getCartQuantity);
+
+
+    // Ejecutamos la función para obtener el número total
+    const totalProductos = getCartQuantity();
+
 
     const handleCloseMenu = () => {
         setMenuOpen(false);
@@ -23,7 +31,8 @@ function Navbar({ menuOpen, setMenuOpen }) {
             <Link to="/admin/cupones">
                 Cupones
             </Link>
-            <Link to="/cart"> 🛒</Link>
+            <Link to="/cart"> 🛒 {totalProductos > 0 && <span>({totalProductos})</span>}
+            </Link>
 
         </nav>
     );
